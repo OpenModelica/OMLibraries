a39c9c67720a9264f83f7ba25f2b49c977d15920
Martin Sjölund <martin.sjolund@liu.se>
2013-09-22 07:49:07 +0200

    Bump library versions

--- check-latest.sh
+++ check-latest.sh
@@ -7,11 +7,11 @@ URL=`svn info $SVNOPTS --xml "$1" | xpath -q -e '/info/entry/url/text()'`
 # `svn info $SVNOPTS --xml "$1" | xpath -q -e '/info/entry/commit/@revision' | grep -o "[0-9]*"`
 CURREV=`cat "$1.rev"`
 
-if svn info $SVNOPTS --xml "$ROOT" >& /dev/null; then
-  URL=$ROOT
-fi
+#if svn info $SVNOPTS --xml "$ROOT" >& /dev/null; then
+#  URL=$ROOT
+#fi
 
 REMOTEREV=`svn info $SVNOPTS --xml "$URL" | xpath -q -e '/info/entry/commit/@revision' | grep -o "[0-9]*"`
-if test "$CURREV" != "$REMOTEREV"; then
+if test "$CURREV" -lt "$REMOTEREV"; then
   echo $1 uses $CURREV but $REMOTEREV is available. Changed paths include `svn log -qv -r$CURREV:$REMOTEREV $URL | egrep -o "(/(tags|branches)/[^/]*/|/trunk/)" | sed "s, (from /,/," | sort -u`
 fi
