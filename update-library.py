d80ab3532065ff8c98dab33723133f39a497b4da
Adrian Pop <adrian.pop@liu.se>
2014-01-08 09:25:53 +0100

    - added sh before the shell script as in Windows python the shell is cmd by default

--- update-library.py
+++ update-library.py
@@ -34,7 +34,7 @@ def update():
     provides[pack] += [p]
   for k in provides.keys():
     f = open(options.build + "/%s.provides" % k,'w')
-    f.write(','.join([subprocess.check_output("./debian-name.sh %s" % item, shell=True).strip() for item in provides[k]]))
+    f.write(','.join([subprocess.check_output("sh ./debian-name.sh %s" % item, shell=True).strip() for item in provides[k]]))
   commands = [updateCommand(r) for r in repos]
   for cmd in commands: print cmd
   try:
