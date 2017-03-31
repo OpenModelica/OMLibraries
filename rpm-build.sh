038f1f9d70e540abdb8796c80407f2a0eb149e04
Martin Sjölund <martin.sjolund@liu.se>
2017-03-31 15:27:23 +0200

    Tweak the alien-generated spec-file to please modern Fedora

--- rpm-build.sh
+++ rpm-build.sh
@@ -13,6 +13,7 @@ if ! echo $1 | grep -q "^.*_.*-1_all[.]deb"; then
   echo "Couldn't extract the version..."
 fi
 
+DIRECTORY=`echo $1 | sed 's/^\(.*\)_\(.*\)-1_all[.]deb$/\1-\2/'`
 VERSION=`echo $1 | sed 's/^.*_\(.*\)-1_all[.]deb$/\1/'`
 RPMVERSION=`echo $VERSION | tr '-' '_'`
 FULLRPMNAME=`echo $1 | sed "s/^\(.*\)_\(.*\)\(-1_all[.]deb\)$/\1-$RPMVERSION-1.noarch.rpm/"`
@@ -32,7 +33,10 @@ rsync `cat .remote/pool`/$1 rpm-build/ || exit 1
 cp .rpmmacros ~/.rpmmacros || exit 1
 
 cd rpm-build || exit 1
-sudo alien -k --to-rpm $1 || exit 1
+sudo alien -g -k --to-rpm $1 || exit 1
+sed -e 's#%dir "/"##' -e 's#%dir "/usr"##' -e 's#%dir "/usr/lib"##' -e 's#%dir "/usr/share"##' -e 's#%dir "/usr/share/doc"##' "`pwd`/$DIRECTORY/"*.spec > tmp.spec || exit 1
+sudo rpmbuild --buildroot="`pwd`/$DIRECTORY/" -bb --target noarch tmp.spec
+mv ../"$FULLRPMNAME" . || exit 1
 if ! test -f "$FULLRPMNAME"; then
   echo "Alien didn't produce $FULLRPMNAME"
   exit 1
